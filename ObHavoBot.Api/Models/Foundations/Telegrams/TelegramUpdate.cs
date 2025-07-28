05e9076a1f08c56d4d42968f072f670eaa0ed3b4
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-28 19:31:20 +0500

    MODELS: Create Telegrams & Weathers

---
+++ ObHavoBot.Api/Models/Foundations/Telegrams/TelegramUpdate.cs
@@ -0,0 +1,9 @@
+﻿namespace ObHavoBot.Api.Models.Foundations.Telegrams
+{
+    public class TelegramUpdate
+    {
+        public long UpdateId { get; set; }
+        public TelegramMessage Message { get; set; }
+        public TelegramCallbackQuery CallbackQuery { get; set; }
+    }
+}
---
+++ ObHavoBot.Api/Models/Foundations/Weathers/Weather.cs
@@ -0,0 +1,10 @@
+﻿namespace ObHavoBot.Api.Models.Foundations.Weathers
+{
+    public class Weather
+    {
+        public string City { get; set; }
+        public DateTime Date { get; set; }
+        public double Temperature { get; set; }
+        public string Description { get; set; }
+    }
+}
